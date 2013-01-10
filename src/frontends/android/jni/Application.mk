d671d23e4bac8d461bedfaad803033788f333dbf
Mikael Magnusson <mikma@users.sourceforge.net>
2013-01-10 02:51:22 +0100

    android: Build mips JNI library

--- src/frontends/android/jni/Application.mk
+++ src/frontends/android/jni/Application.mk
@@ -1,5 +1,4 @@
 APP_PLATFORM=android-14
-# TODO add mips x86
-APP_ABI := armeabi x86
+APP_ABI := armeabi x86 mips
 APP_PROJECT_PATH := $(call my-dir)/..
 APP_BUILD_SCRIPT := $(call my-dir)/Android.mk
